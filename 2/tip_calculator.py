3fde1cdd03b7d0697b82e5639bb3d5627310756f
alexcato <alexander.calderontorres@ucr.ac.cr>
2022-03-13 16:10:42 -0600

    Day 2 finished: Tip calculator

---
+++ 2/tip_calculator.py
@@ -0,0 +1,11 @@
+# TIP CALCULATOR
+
+total = float(input("What is the total amount to pay?\n\t> "))
+
+tip_percentage = int(input("What percentage would you like to tip? (10, 12, 15)\n\t> "))
+
+people = int(input("How many people are going to pay the bill?\n\t> "))
+
+total_per_person = ( total + total*(tip_percentage/100) )/ people
+
+print(f"Each person should pay ${total_per_person}")
